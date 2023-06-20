e675ff311355d48f632504be8c455e838fb86029
bruno guilherme de morais <bruno.gmmarques@senacsp.edu.br>
2023-06-20 15:27:48 -0300

    auladeCsharp

---
+++ calculodemedia/calculodemedia/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculodemedia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double numero1, numero2, numero3, numero4, media;
+
+            Console.Write("digite nota 1: ");
+            numero1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("digite nota 2: ");
+            numero2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("digite nota 3: ");
+            numero3 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("digite nota 4: ");
+            numero4 = Convert.ToDouble(Console.ReadLine());
+
+            
+            media = (numero1 + numero2 + numero3 + numero4) / 4;
+            
+            Console.WriteLine("========================================");
+           
+            if (media >= 7)
+            {
+                Console.WriteLine("aluno aprovado");
+            }
+            else
+            {
+                Console.WriteLine("aluno reprovado");
+            }
+            Console.WriteLine("========================================");
+
+            
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
